cea402eee9b0084d9404b30ce8ee2d7146ad53ff
enPiT-P31 <d802@oshima-k.ac.jp>
2018-09-17 17:47:31 +0900

    第１１章終了いたしました

---
+++ app/src/main/java/com/example/enpit_p12/myalarmclock/AlarmBroadcastReceiver.kt
@@ -0,0 +1,17 @@
+package com.example.enpit_p12.myalarmclock
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.newTask
+
+
+class AlarmBroadcastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        context?.run {
+            startActivity(
+                    intentFor<MainActivity>("onReceive" to true).newTask())
+        }
+    }
+}
